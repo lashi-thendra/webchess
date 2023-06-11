c74e87e2eb84e093d35b7a5ca31e9a2594a8b6ae
Lashi Thendra <lashithendrabandara@gmail.com>
2023-06-11 20:30:18 +0530

    completed the ui

--- js/view.js
+++ js/view.js
@@ -6,19 +6,28 @@ $('.square').each((i, element)=>{
     $(element).attr('data-row', rowNumber);
     $(element).attr('data-col', colNumber );
     $(element).addClass(((i+1+ Math.floor((i)/8))%2==0)? "black": "white");
-    $(element).addClass( `row-${rowNumber} col-${colNumber}`);
+    $(element).addClass( `rc-${rowNumber}-${colNumber}`);
 });
 
 // add pieces
-$('.row-2').each((i,element)=>{
-    addPiece('white','pawn',$(element));
-});
-
-$('.row-7').each((i,element)=>{
-    addPiece('black','pawn',$(element));
-});
-
-
+for (let i = 1; i <= 8; i++) {
+    let pieceName;
+    switch(i){
+        case 1: pieceName = 'rook'; break;
+        case 8: pieceName = 'rook'; break;
+        case 2: pieceName = 'bishop'; break;
+        case 7: pieceName = 'bishop'; break;
+        case 3: pieceName = 'knight'; break;
+        case 6: pieceName = 'knight'; break;
+        case 4: pieceName = 'queen'; break;
+        case 5: pieceName = 'king'; break;
+    }
+    
+    addPiece('white', pieceName,$(`.rc-${1}-${i}`) );
+    addPiece('black', pieceName,$(`.rc-${8}-${i}`) );
+    addPiece('white', 'pawn',$(`.rc-${2}-${i}`) );
+    addPiece('black', 'pawn',$(`.rc-${7}-${i}`) );
+}
 
 
 $('.piece').each((i,elm)=>{
